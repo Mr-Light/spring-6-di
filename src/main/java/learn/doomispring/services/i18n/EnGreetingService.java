10c104e1af78364c40bde3b3ae5f5e9402ed5388
a_hawaryy <adham.elhawary1@gmail.com>
2024-11-13 19:09:04 +0200

    default option preview

--- src/main/java/learn/doomispring/services/i18n/EnGreetingService.java
+++ src/main/java/learn/doomispring/services/i18n/EnGreetingService.java
@@ -4,7 +4,8 @@ import learn.doomispring.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("en")
+// default is a special type of profile that can mean if no profile is active use me
+@Profile({"en", "default"})
 @Service("i18nService")
 public class EnGreetingService implements GreetingService {
     @Override
