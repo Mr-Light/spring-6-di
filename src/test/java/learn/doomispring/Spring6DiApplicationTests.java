2299a550226878dda7b4333b1014110823e0f53f
a_hawaryy <adham.elhawary1@gmail.com>
2024-11-13 09:50:04 +0200

    Setting it up

---
+++ src/test/java/learn/doomispring/Spring6DiApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.doomispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6DiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
